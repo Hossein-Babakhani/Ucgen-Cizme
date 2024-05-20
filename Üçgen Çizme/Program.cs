43638ac5454fe4919174bc317a9589a74a34253e
Hossein Babakhani <bna.babakhani@gmail.com>
2024-05-20 20:28:06 +0300

    Add files via upload

---
+++ Üçgen Çizme/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Üçgen_Çizme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Üçgenin boyutunu girin:");
+            int boyut = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= boyut; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
